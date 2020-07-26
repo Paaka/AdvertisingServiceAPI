8a0d559f07bea52aa131d2e8bdb3aaa0ad96bec4
Paaka <32105930+Paaka@users.noreply.github.com>
2020-07-26 15:25:04 +0200

    Added express-graphql

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressGraphQL = require('express-graphql').graphqlHTTP;
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/',(req, res, next) => {
-    res.send('Work ?');
-})
+app.use('/graphql',
+    expressGraphQL({
+    schema:buildSchema(`
+        type RootQuery {
+            advertises: [String!]!
+        }
+
+        type RootMutation {
+            createAdvertise(name:String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue:{
+        advertises: () => {
+            return ['Toyota Celica', 'Fiat 500'];
+        },
+
+        createAdvertise: (args) => {
+            const advertiseName = args.name;
+
+            return advertiseName;
+        }
+    },
+    graphiql:true,
+}))
 
 app.listen(8000);
