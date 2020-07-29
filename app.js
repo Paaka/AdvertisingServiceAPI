48b807f342e5ee090cd142cd9d4d0acecb3bac73
Paaka <32105930+Paaka@users.noreply.github.com>
2020-07-29 12:48:33 +0200

    Added database

--- app.js
+++ app.js
@@ -2,20 +2,38 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const expressGraphQL = require('express-graphql').graphqlHTTP;
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Advertise = require('./models/advertise');
 
 const app = express();
+
 
 app.use(bodyParser.json());
 
 app.use('/graphql',
     expressGraphQL({
     schema:buildSchema(`
+        type Advertise{
+            _id: ID!
+            title:String!
+            description:String!
+            price:Float!
+            date:String!
+        }
+
+        input AdvertiseInput {
+            title:String!
+            description:String!
+            price:Float!
+        }
+
         type RootQuery {
-            advertises: [String!]!
+            advertises: [Advertise!]!
         }
 
         type RootMutation {
-            createAdvertise(name:String): String
+            createAdvertise(advertiseInput:AdvertiseInput): Advertise
         }
 
         schema {
@@ -25,16 +43,46 @@ app.use('/graphql',
     `),
     rootValue:{
         advertises: () => {
-            return ['Toyota Celica', 'Fiat 500'];
+            return Advertise
+                        .find({})
+                        .then(adverises => {
+                            return adverises.map(advert =>{
+                                return {...advert._doc}
+                            })
+                        })
+                        .catch(err => {
+                            throw err;
+                        });
         },
 
         createAdvertise: (args) => {
-            const advertiseName = args.name;
+            const advertise = new Advertise({
+                title: args.advertiseInput.title,
+                description:args.advertiseInput.description,
+                price:args.advertiseInput.price,
+                date: new Date(),
+            });
 
-            return advertiseName;
+            return advertise
+                .save()
+                .then(res => {
+                    console.log(res);
+                    return {...res._doc}
+                })
+                .catch(err => {
+                    console.log(err);
+                    throw err;
+                })
+
+            
         }
     },
     graphiql:true,
 }))
 
-app.listen(8000);
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${
+    process.env.MONGO_PASSWORD
+    }@graphqldb.ddrph.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+    .then(()=> app.listen(8000) )
+    .catch(err => console.log(err))
+
